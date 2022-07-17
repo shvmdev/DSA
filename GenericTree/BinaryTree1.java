e8d0ea6ffdc5af7fdd92b3bd2b50959fe6731e8e
Shivam Gupta <200709shivam@gmail.com>
2022-07-17 08:16:06 -0700

    BinaryTree done till display

--- GenericTree/BinaryTree1.java
+++ GenericTree/BinaryTree1.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
 public class BinaryTree1 {
+    public static class Node{
+        static int data;
+        static Node next;
+        Node(int data){
+            Node.data=data;
+            Node.next=null;
+        }
+    }
     public static void main(String[] args){
         Scanner scn=new Scanner(System.in);
         String s=scn.next();
