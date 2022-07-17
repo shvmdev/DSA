e936ef57c0db3a550544eb8e8f93f343dfbbdd8c
Shivam Gupta <200709shivam@gmail.com>
2022-07-17 08:04:29 -0700

    BinaryTree firstattempt done

---
+++ GenericTree/BinaryTree1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class BinaryTree1 {
+    public static void main(String[] args){
+        Scanner scn=new Scanner(System.in);
+        String s=scn.next();
+        System.out.println("Hello, "+s );
+    }
+}
